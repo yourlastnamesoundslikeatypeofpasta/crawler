6e6fa4e832c4e996f82f36a90245df0fc0fb2084
czagazeta <christianzagazeta@icloud.com>
2019-08-18 17:57:55 -0400

    Fixed! base_url now does what its intended to do...return the base of a url. Added comments. Added some better logic and removed redundant logic.

--- base_url.py
+++ base_url.py
@@ -7,26 +7,24 @@ def base_url(url):
     :param url: Raw URL: http://google.com/q=dog%sprinting%on%treadmill
     :return: http://google.com
     """
-
+    # List of preferred domains the URL should end with.
     ext_lst = ['org', 'com', 'edu', 'net', 'gov', '.us',
 
                '.mx', 'mil', '.ca', 'business.site', 'nfo',
                'health', 'biz', '.br', '.life', '.au']
-    if url[:3] != 'http': # FIXME: function is returning: http://https:
-                        #       and then going in an infinite loop in the proceeding while loop
+
+    # Add 'http://' if the url is missing scheme
+    if url.startswith('http://') is False and url.startswith('https://') is False:
         url = 'http://' + url
+
+    # Split out the base url from the url
     parts = urlsplit(url)
     url_base = "{0.scheme}://{0.netloc}".format(parts)
-    print(url_base)
+
+    # Check if the base url ends with a preferred extension domain and return the base url
     while len(url_base) > 3:
         for ex in ext_lst:
             if url_base.endswith(ex):
                 return url_base
-
-        # base_url_list = []
-        # for char in base_url[::-1]:
-        #     base_url_list.append(char)
-        # url_base = ''.join(base_url_list)
-    return url_base
-
-print(base_url('https://www.smh.com.au/contact-us'))
+        print(f'URL Extension: {url_base[:-3]} is not in extension list.')
+        return url_base
