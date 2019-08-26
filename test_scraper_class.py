63351f5c700960294bd262ce7a9b448963ff1701
yourlastnamesoundslikeatypeofpasta <christianzagazeta@icloud.com>
2019-08-26 02:33:54 -0400

    Testing out unit testing

--- test_scraper_class.py
+++ test_scraper_class.py
@@ -4,31 +4,39 @@ from scraper_class import Scraper
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    # def test_something(self):
+    #     self.assertEqual(True, True)
 
-    def test_new_urls(self):
-        url = Scraper('https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/')
-        self.assertEqual(
-            url.new_urls,
-            deque(['https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/'])
-        )
+    # def test_new_urls(self):
+    #     url = Scraper('https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/')
+    #     self.assertEqual(
+    #         url.new_urls,
+    #         deque(['https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/'])
+    #     )
 
-    def test_init_dictionaries(self):
-        url = Scraper('https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/')
-        self.assertEqual(len(url.email_dict), 1)
-        self.assertEqual(len(url.url_counter), 1)
-        self.assertEqual(len(url.queue_counter), 1)
+    # def test_init_dictionaries(self):
+    #     url = Scraper('https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/')
+    #     self.assertEqual(len(url.email_dict), 1)
+    #     self.assertEqual(len(url.url_counter), 1)
+    #     self.assertEqual(len(url.queue_counter), 1)
 
-    @ staticmethod
-    def test_print_emails():
+    def test_get_email_with_html_parser(self):
         url = Scraper('https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/')
         url.set_current_url()
-        url.email_dict[url.get_current_base_url()] = ['chris@cb.com', 'beth@cb.com']
-        url.email_dict.setdefault('bethsblueberrypies.com', ['beth@pie.com', 'chris@pie.com'])
+        url.set_response_with_html()
+        url.get_email_from_response()
+        self.assertEqual(1, len(url.email_dict))
         url.print_emails()
+
+    # @ staticmethod
+    # def test_print_emails():
+    #     url = Scraper('https://cannabistheorys.com/cannabis-theorys-muv-product-tutorial-11-thc-cbd-inhaler/')
+    #     url.set_current_url()
+    #     url.email_dict[url.get_current_base_url()] = ['chris@cb.com', 'beth@cb.com']
+    #     url.email_dict.setdefault('bethsblueberrypies.com', ['beth@pie.com', 'chris@pie.com'])
+    #     url.print_emails()
 
 
 if __name__ == '__main__':
     # unittest.main()
-    MyTestCase.test_print_emails
+    MyTestCase.test_get_email_with_html_parser()
