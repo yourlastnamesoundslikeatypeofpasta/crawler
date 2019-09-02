b03833d41cfeae2c281929648fa5c37a254f6753
czagazeta <christianzagazeta@icloud.com>
2019-09-02 14:17:35 -0400

    more testing

--- multiprocess_practice/multiporcessing_pool.py
+++ multiprocess_practice/multiporcessing_pool.py
@@ -16,14 +16,15 @@ if __name__ == '__main__':
     print('Input   :', inputs)
 
     builtin_outputs = map(do_calculation, inputs)
-    print('Built-in:', builtin_outputs)
+    print('Built-in:', list(builtin_outputs))
 
     pool_size = multiprocessing.cpu_count() * 2
     pool = multiprocessing.Pool(
         processes=pool_size,
         initializer=start_process,
-        maxtasksperchild=2
+
     )
+
     pool_outputs = pool.map(do_calculation, inputs)
     pool.close()  # no more tasks
     pool.join()  # wrap up current tasks
