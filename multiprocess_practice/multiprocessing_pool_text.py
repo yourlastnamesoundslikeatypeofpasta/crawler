a24116d0869fdfbacca8ae5aa3e7ca9d0fd4d420
czagazeta <christianzagazeta@icloud.com>
2019-09-02 08:17:47 -0400

    testing out multiprocessing with the crawler inital commit

---
+++ multiprocess_practice/multiprocessing_pool_text.py
@@ -0,0 +1,13 @@
+import multiprocessing
+from crawl import Crawl
+
+
+def crawl(session_name, url):
+    pass
+
+
+def start_crawl():
+    print(f'Starting', multiprocessing.current_process().name)
+
+if __name__ == '__main__':
+    session
