88db93fe8f00c3147172e8fdb5313fc46dd2d148
yourlastnamesoundslikeatypeofpasta <christianzagazeta@icloud.com>
2019-09-05 12:02:23 -0400

    Added a docstring Updated function to correctly return url names of sites in the following formats:   https://www.smg.com   books.toscrape.com

--- get_website_name.py
+++ get_website_name.py
@@ -2,6 +2,21 @@ from urllib.parse import urlsplit
 
 
 def get_web_name(url):
+    """
+    Get the name of a website.
+    :param url: the url to get the name from
+    :return: a string of the name of the website
+    """
     parts = urlsplit(url)
-    network_location = '{0.netloc}'.format(parts).split('.')[0]
-    return network_location
+    name_list = '{0.netloc}'.format(parts).split('.')
+    web_name = ''
+
+    # allow this function to create names that have multiple '.' or 'www' in them
+    for name in name_list:
+        if name.startswith('www') or name.startswith('com'):
+            continue
+        if web_name:
+            web_name = web_name + '.' + name
+        else:
+            web_name = name
+    return web_name
