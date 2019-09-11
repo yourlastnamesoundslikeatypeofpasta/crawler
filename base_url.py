966acc7efdb94daf67953cf1eecd42a35fc8e2bf
yourlastnamesoundslikeatypeofpasta <christianzagazeta@icloud.com>
2019-09-11 04:52:25 -0400

    After learning a little more about urllib.parse I have removed a lot of the fluff that that this function contained.

--- base_url.py
+++ base_url.py
@@ -8,30 +8,12 @@ def base_url(url):
     :param url: Raw URL: http://google.com/q=dog%sprinting%on%treadmill
     :return: http://google.com
     """
-    # List of preferred domains the URL should end with.
-    ext_lst = ['org', 'com', 'edu', 'net', 'gov', '.us',
-
-               '.mx', 'mil', '.ca', 'business.site', 'nfo',
-               'health', 'biz', '.br', '.life', '.au']
-
     # Add 'http://' if the new_urls is missing scheme
     if url.startswith('http://') is False and url.startswith('https://') is False:
         url = 'http://' + url
 
     # Split out the base new_urls from the new_urls
     parts = urlsplit(url)
-    url_base = "{0.scheme}://{0.netloc}".format(parts)
+    url_base = f"{parts.scheme}://{parts.netloc}/"
 
-    # Check if the base new_urls ends with a preferred extension domain and return the base new_urls
-    while len(url_base) > 3:
-        for ex in ext_lst:
-            if url_base.endswith(ex):
-                # Add a trailing foreword slash if the the link doesn't have one
-                if url_base.endswith('/') is False:
-                    url_base = url_base + '/'
-                return url_base
-        print(f'URL Extension: {url_base[-4:]} is not in extension list for {url_base}')
-        # Add a trailing foreword slash if the the link doesn't have one
-        if url_base.endswith('/') is False:
-            url_base = url_base + '/'
-        return url_base
+    return url_base
