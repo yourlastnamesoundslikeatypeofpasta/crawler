5baeb8055a0891b9fe7adde90be0b68faf7f026b
yourlastnamesoundslikeatypeofpasta <MC3Er64ziP>
2019-10-02 13:43:10 -0400

    Quick Squeeze Added methods:   quick_squeeze --> quickly check if new_urls contains file types that   the user is looking for Updated Methods:   downloader --> fixing stdout issues   crawl --> fixing stdout issues, and added quick_squeeze to KeyboardInterrupt statement

--- link_filetype.py
+++ link_filetype.py
@@ -59,26 +59,32 @@ class LinkFileType(Crawl):
         """
         # TODO: print--> amount of each file type found
         # TODO: ask the user if they would like to download all of the files
-        for file_num, url in enumerate(self.result_list):
-            file_num += 1
-            print(f'Downloading Files: {round((file_num / len(self.result_list)) * 100)}%', end='\r')
+        download_q = input(f'Download Found Files: {len(self.result_list)}<yes|no>').lower()
+        if 'y' in download_q:
+            print(f'Downloading {len(self.result_list)} files...')
+            for file_num, url in enumerate(self.result_list):
+                file_num += 1
+                print(f'Downloading File: {file_num} of {len(self.result_list)}|Complete: {round((file_num / len(self.result_list)) * 100)}%', end='\r')
+                # print(f'\rDownloading Files: {round((file_num / len(self.result_list)) * 100)}%')
 
-            # get the file name
-            path = urlparse(url).path
-            path_split = os.path.split(path)
-            file_name = path_split[-1]
+                # get the file name
+                path = urlparse(url).path
+                path_split = os.path.split(path)
+                file_name = path_split[-1]
 
-            # check if the session folder name exists, create it if not
-            dir_path = f'./downloads/{self.session_name}'
-            if not os.path.exists(dir_path):
-                os.mkdir(dir_path)
+                # check if the session folder name exists, create it if not
+                dir_path = f'./downloads/{self.session_name}'
+                if not os.path.exists(dir_path):
+                    os.mkdir(dir_path)
 
-            # download the file
-            r = requests.get(url, stream=True)
-            with open(f'{dir_path}/{file_name}', 'wb') as f:
-                f.write(r.content)
-        print('Downloading Complete', end='\r')
-        print(f'Number of files downloaded: {len(self.result_list)}')
+                # download the file
+                r = requests.get(url, stream=True)
+                with open(f'{dir_path}/{file_name}', 'wb') as f:
+                    f.write(r.content)
+            print('Downloads Complete', end='\r')
+            print(f'Downloads Completed|Number of files downloaded: {len(self.result_list)}')
+        else:
+            print(f'Download Aborted')
 
     def add_result(self):
         """
@@ -86,6 +92,38 @@ class LinkFileType(Crawl):
         :return: None
         """
         self.result_list.append(self.current_url)
+
+    def quick_squeeze(self):
+        """
+        Go through the new_urls list and check if any of the file types provided are in the url.
+        This method isn't 100% accurate as some file types aren't included in the url so this method is intended
+        to be used as a cast net.
+        :return: None
+        """
+        print('Performing a quick-squeeze!')
+        found_list = []
+        found_count = 0
+        # for each file_type, check if the file_type is in any of the urls in new_urls
+        for file_type in self.file_type:
+            for url in self.new_urls:
+                if file_type in url:
+                    found_count += 1
+                    found_list.append(url)
+
+        # check if each url has the correct content-type
+        print(f'confirming {found_count} files...this may take awhile...')
+        confirm_count = 0
+        for url in found_list:
+            r = requests.get(url).headers['content-type']
+            for file_type in self.file_type:
+                if file_type in r:
+                    self.result_list.append(url)
+                    confirm_count += 1
+            stdout.write(f'\rConfirmed: {confirm_count}/{found_count}')
+
+        # remove urls from new_urls
+        for url in found_list:
+            self.new_urls.remove(url)
 
     def check_file_type(self, response):
         # check file type
@@ -133,36 +171,6 @@ class LinkFileType(Crawl):
                 requests.exceptions.Timeout, requests.exceptions.TooManyRedirects) as e:
             self.response = None
 
-    def crawl(self):
-        session = self.session_name
-        status = 'crawling'
-        try:
-            while len(self.new_urls):
-                self.set_current_url()
-                queue = len(self.new_urls)
-                processing = self.current_url
-                stdout.write(
-                    f'|Session:{session}|Status:{status}|Queue:{queue}|Files Found: {len(self.result_list)}|Processing:{processing}\r')
-                self.set_response_with_html()
-                if self.response:
-                    self.get_new_urls_from_html()
-                    time.sleep(self.sleep_time)
-                self.save_progress()
-                time.sleep(.1)
-
-            # crawl complete
-            self.save_progress()
-            # print(f'Session - {self.session_name}')
-            status = 'crawl complete'
-            print(f'|Session:{session}|Status:{status}|')
-            self.downloader()
-            # self.print_buggy_links()
-        except KeyboardInterrupt:
-            self.print_buggy_links()
-        return {'session_name': self.session_name,
-                'url_counter': self.url_counter,
-                'debug_dict': self.debug_dict}
-
     def save_progress(self):
         """
         Save the variables new_urls, response, current_url, poss_link,
@@ -206,3 +214,41 @@ class LinkFileType(Crawl):
         for k, v in attribute_dict.items():
             setattr(url, k, v)
         return url
+
+    def crawl(self):
+        session = self.session_name
+        status = 'CRAWLING...'
+        try:
+            # begin crawling
+            while self.new_urls:
+                self.set_current_url()
+                queue = len(self.new_urls)
+                processing = self.current_url
+                scanned = len(self.new_urls)
+                file_found = len(self.result_list)
+                # p = f'|Session:{session}|Status:{status}|Queue:{queue}|Files Found: {len(self.result_list)}|Processing:{processing}\r'
+                p = f'|Session:{session}|Status:{status}|Scanned:{scanned}|Queue:{queue}|Files Found:{file_found}|\r'
+                stdout.write(p)
+                stdout.flush()
+                self.set_response_with_html()
+                if self.response:
+                    self.get_new_urls_from_html()
+                    time.sleep(self.sleep_time)
+                self.save_progress()
+                # time.sleep(.1)
+
+            # crawl complete
+            self.save_progress()
+            # print(f'Session - {self.session_name}')
+            status = 'crawl complete'
+            print(f'|Session:{session}|Status:{status}|')
+            self.downloader()
+            # self.print_buggy_links()
+        except KeyboardInterrupt:
+            # perform a quick squeeze
+            self.quick_squeeze()
+            self.downloader()
+            self.print_buggy_links()
+        return {'session_name': self.session_name,
+                'url_counter': self.url_counter,
+                'debug_dict': self.debug_dict}
